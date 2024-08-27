2edf6b706b708febf5da37510ee3d4886a26485c
carjavi <carjavi@hotmail.com>
2024-08-27 02:06:00 -0400

    upgrade

--- create-autorun-service.sh
+++ create-autorun-service.sh
@@ -1,19 +1,3 @@
-<p align="center"><img src="./img/bashlogo.png" width="400"  alt=" " /></p>
-<h1 align="center">Run a program on your Raspberry Pi at Startup with Daemon Service SYSTEMD</h1> 
-<h4 align="right">Aug 24</h4>
-
-<img src="https://img.shields.io/badge/Hardware-Raspberry%20ver%204-red">
-<img src="https://img.shields.io/badge/Hardware-Raspberry%203B%2B-red">
-<img src="https://img.shields.io/badge/Hardware-Raspberry%20Zero-red">
-
-<br>
-Este script permite crear de forma interactiva un servicio que se iniciara cada vez que se inicie la raspberry.
-
-
-<br>
-
-create-autorun-service.sh
-```
 #!/usr/bin/env bash
 
 
@@ -125,58 +109,3 @@ echo "-------------------------------------"
 echo
 
 sleep 10 && reboot
-
-```
-
-<br>
-
-> :warning: **Warning:** Para correr el archivo hay que darle permiso de ejecución:
-```
-sudo chmod +x create-autorun-service.sh
-```
-
-<br>
-
-# Daemon Service SYSTEMD commands (summary)
-```
-sudo systemctl daemon-reload
-sudo systemctl status SERVICE_NAME
-sudo systemctl stop SERVICE_NAME
-sudo systemctl start SERVICE_NAME
-sudo systemctl disable SERVICE_NAME
-sudo systemctl enable SERVICE_NAME
-sudo systemctl restart SERVICE_NAME
-```
-# Edit Service
-> :warning: **Warning:** ```sudo systemctl daemon-reload```deberá ingresar este  comando cada vez que cambie su archivo .service,ya que systemd necesita saber que se ha actualizado.
-```
-sudo nano /etc/systemd/system/SERVICE_NAME
-```
-
-# Debugging
-La salida de systemd (por ejemplo, sentencias print() o mensajes de error) es capturada por el sistema journalctl y se puede ver con el siguiente comando:
-```
-sudo journalctl -f -u SERVICE_NAME
-```
-Esto puede dar una idea de lo que está pasando con su servicio o programa.
-
-# Delete service
-```
-sudo rm /lib/systemd/system/clock.service
-sudo systemctl daemon-reload
-sudo reboot
-```
-
-
-more info: https://github.com/carjavi/raspberry-pi-guide?tab=readme-ov-file#daemon-service-systemd
-
-<br>
-
----
-Copyright &copy; 2022 [carjavi](https://github.com/carjavi). <br>
-```www.instintodigital.net``` <br>
-carjavi@hotmail.com <br>
-<p align="center">
-    <a href="https://instintodigital.net/" target="_blank"><img src="./img/developer.png" height="100" alt="www.instintodigital.net"></a>
-</p>
-
